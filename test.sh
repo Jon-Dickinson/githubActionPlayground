1a5cb7fcf07c05f700756353e1945b5481f8d74a
pouannes <pierreouannes@gmail.com>
2021-03-28 19:34:28 +0200

    now it should work!

--- test.sh
+++ test.sh
@@ -1,6 +1,16 @@
-commitList=`git log --format=%B ca40...695b60`
+commitList=`git log --format=%B ca40...a744814009e`
+flag=false
 while IFS= read -r line; do
-    if [ ${#line} -ge 5 ] && [ ${#line} -eq 0]; then echo "line '$line' is greater than 5 chars"
-    else echo "line '$line' is long enough"
+    if [ ${#line} -ge 5 ]; then 
+        echo "line '$line' is greater than 5 chars"
+    elif [ -z ${line} ]; then
+        echo "line '$line'  is empty"
+    else 
+        echo "line '$line' is not long enough, setting the flag to true!"
+        flag=true
     fi
 done <<< "$commitList"
+echo $flag
+if [ "$flag" = true ]; then
+    echo "do the curl command"
+fi
